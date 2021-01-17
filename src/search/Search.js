91bb64d0a6815d5ec6369bfdac4ae519c5903842
Danny Carcamo <dannycarcamo@icloud.com>
2021-01-17 17:44:08 -0600

    adjustments

--- src/search/Search.js
+++ src/search/Search.js
@@ -11,21 +11,21 @@ export default function Search(props) {
                     className="search" 
                     type="text" />
                 <button type="submit">Search</button>
+                <div className="results-filter">
+                <label htmlFor="print-filter" className="print-type">Print Type: </label>
+                <select className="print-filter"  onChange={props.handlePrintType} >
+                    <option value="">All</option>
+                    <option value="books">Books</option>
+                    <option value="magazines">Magazines</option>
+                </select>
+                <label htmlFor="book-filter" className="book-type">Book Type: </label>
+                <select className="book-filter"  onChange={props.handleFilter} >
+                    <option value="">No Filter</option>
+                    <option value="free-ebooks">Free eBooks</option>
+                    <option value="paid-ebooks">Paid eBooks</option>
+                </select>
+                </div>
             </form>
-            <div className="results-filter">
-            <label htmlFor="print-filter" className="print-type">Print Type: </label>
-            <select className="print-filter"  onChange={props.handlePrintType} >
-                <option value="">All</option>
-                <option value="books">Books</option>
-                <option value="magazines">Magazines</option>
-            </select>
-            <label htmlFor="book-filter" className="book-type">Book Type: </label>
-            <select className="book-filter"  onChange={props.handleFilter} >
-                <option value="">No Filter</option>
-                <option value="free-ebooks">Free eBooks</option>
-                <option value="paid-ebooks">Paid eBooks</option>
-            </select>
-        </div>
         </div>
     )
 }
